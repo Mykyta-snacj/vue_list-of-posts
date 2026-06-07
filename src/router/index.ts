33b71e7bd12a6f55ae960eebf6a9f06b2e4b4d4e
Mykyta Kalniev <nikitamal4ik@gmail.com>
2026-06-07 15:50:51 +0200

    add a task solution

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import LoginPage from '@/pages/LoginPage.vue'
-import PostsPage from '@/pages/PostsPage.vue'
+import LoginPage from '../pages/LoginPage.vue'
+import PostsPage from '../pages/PostsPage.vue'
 
 const routes = [
   {
