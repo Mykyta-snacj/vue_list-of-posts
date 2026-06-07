a2519a70a2b997d45ae0b4e239bdf769fb377b18
Mykyta Kalniev <nikitamal4ik@gmail.com>
2026-06-07 15:30:55 +0200

    add a task solution

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,3 @@
-<script lang="ts">
-//components
-import MainNavbar from './components/MainNavbar.vue'
-import PostsSection from './components/PostsSection.vue'
-
-export default {
-  components: {
-    MainNavbar,
-    PostsSection,
-  },
-}
-</script>
-
 <template>
-  <div id="root">
-    <MainNavbar />
-
-    <main class="section">
-      <div class="container">
-        <PostsSection />
-      </div>
-    </main>
-  </div>
+  <RouterView />
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+import LoginPage from '@/pages/LoginPage.vue'
+import PostsPage from '@/pages/PostsPage.vue'
 
-export default router
+const routes = [
+  {
+    path: '/',
+    redirect: '/login',
+  },
+  {
+    path: '/login',
+    component: LoginPage,
+  },
+  {
+    path: '/posts',
+    component: PostsPage,
+  },
+]
+
+export const router = createRouter({
+  history: createWebHistory('/vue_list-of-posts/'),
+  routes,
+})
